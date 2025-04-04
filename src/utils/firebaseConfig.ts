68db4f67aba1b3ba38a08ee85bcd0a465b5ddf71
pranavviz <104368801+pranavviz@users.noreply.github.com>
2025-04-04 16:18:18 +0530

    Update firebaseConfig.ts

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-pT67WqhlVaXUgeYWqnDr4AgBV6_lC8Y",
-  authDomain: "pokedex-pranav.firebaseapp.com",
-  projectId: "pokedex-pranav",
-  storageBucket: "pokedex-pranav.firebasestorage.app",
-  messagingSenderId: "145363872912",
-  appId: "1:145363872912:web:6cd4c1f7bae95255bb7c15",
-  measurementId: "G-8G8BZ1KDJW",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
